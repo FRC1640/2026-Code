8926028d0c2f3e9fcf485cf0c65f64c0dd01ec8d
1023941311414 <jake.widmann@outlook.com>
2026-01-23 19:39:53 -0500

    Hopper IO shell

--- src/main/java/frc/robot/subsystems/hopper/HopperIO.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperIO.java
@@ -1,5 +1,31 @@
 package frc.robot.subsystems.hopper;
 
-public class HopperIO {
-  
+import org.littletonrobotics.junction.AutoLog;
+
+import frc.robot.Robot;
+
+public interface HopperIO extends AutoCloseable{
+  @AutoLog
+  public class HopperIOInputs{
+    public double HopperAngle;
+    public double HopperMotorTemperature;
+    public double HopperMotorCurrent;
+    public double HopperMotorVoltage;
+  }
+
+  public default void setHopperVoltage(double voltage) {}
+
+  public default void updateInputs(HopperIOInputs inputs) {}
+
+  @Override
+  public default void close() {
+  }
+
+  public static HopperIO getIOByMode() {
+    return switch (Robot.getMode()) {
+      case REAL -> new HopperIOReal();
+      case SIM -> new HopperIOSim();
+      case REPLAY -> new HopperIO() {};
+    };
+  }
 }
