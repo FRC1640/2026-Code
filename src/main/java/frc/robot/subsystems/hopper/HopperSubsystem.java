4e92c495f757a1be56f949fbf8cb814d2b00a6d1
1023941311414 <jake.widmann@outlook.com>
2026-01-23 19:38:23 -0500

    Hopper subsystem shell

--- src/main/java/frc/robot/subsystems/hopper/HopperSubsystem.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperSubsystem.java
@@ -1,5 +1,20 @@
 package frc.robot.subsystems.hopper;
 
-public class HopperSubsystem {
-  
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class HopperSubsystem extends SubsystemBase {
+  private HopperIO io;
+  private HopperIOInputsAutoLogged inputs = new HopperIOInputsAutoLogged();
+
+  public HopperSubsystem(HopperIO io) {
+    this.io = io;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Hopper", inputs);
+  }
 }
