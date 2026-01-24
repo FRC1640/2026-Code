531e2ecc928e846f135f30fdebc460810e034c05
1023941311414 <jake.widmann@outlook.com>
2026-01-23 19:41:26 -0500

    IO implementation shells

--- src/main/java/frc/robot/subsystems/hopper/HopperIOReal.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperIOReal.java
@@ -1,5 +1,14 @@
 package frc.robot.subsystems.hopper;
 
-public class HopperIOReal {
-  
+public class HopperIOReal implements HopperIO {
+
+  public HopperIOReal() {}
+
+  @Override
+  public void setHopperVoltage(double angle) {}
+
+  @Override
+  public void updateInputs(HopperIOInputs inputs){
+
+  }
 }
--- src/main/java/frc/robot/subsystems/hopper/HopperIOSim.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperIOSim.java
@@ -1,5 +1,14 @@
 package frc.robot.subsystems.hopper;
 
-public class HopperIOSim {
-  
+public class HopperIOSim implements HopperIO {
+
+  public HopperIOSim() {}
+
+  @Override
+  public void setHopperVoltage(double angle) {}
+
+  @Override
+  public void updateInputs(HopperIOInputs inputs){
+
+  }
 }
