c9b3265a228a3e6a1ee514b619a01bd435147837
Aryan Mitra <aryan9mitra@gmail.com>
2026-01-24 15:27:43 -0500

    added variables

--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -1,5 +1,10 @@
 package frc.robot.subsystems.intake;
 
 public class IntakeIO {
-    
+    public static class IntakeIOInputs {
+        public double voltage; 
+        public double temperature;
+        public double current;
+    }
 }
+
--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -1,5 +1,4 @@
 package frc.robot.subsystems.intake;
 
 public class IntakeIOSim {
-    
 }
