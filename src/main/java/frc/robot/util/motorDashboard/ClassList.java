d316fb2e55761465bdb3948b34a855d2cd622331
tectonic1010 <159208901+tectonic1010@users.noreply.github.com>
2026-01-24 16:23:22 -0500

    uhhh starting smth class list idk might remove potentially

---
+++ src/main/java/frc/robot/util/motorDashboard/ClassList.java
@@ -0,0 +1,12 @@
+package frc.robot.util.motorDashboard;
+
+import frc.robot.subsystems.drive.DriveSubsystem;
+
+public class ClassList {
+    //TODO: actually put the right subsystems
+    private DriveSubsystem driveSubsystem;
+    
+    public ClassList(DriveSubsystem driveSubsystem){
+        this.driveSubsystem = driveSubsystem;
+    }
+}
--- src/main/java/frc/robot/util/motorDashboard/Dashboard.java
+++ src/main/java/frc/robot/util/motorDashboard/Dashboard.java
@@ -1,17 +1,19 @@
 package frc.robot.util.motorDashboard;
 
+import java.util.List;
+
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class Dashboard {
     private static Dashboard instance;
-    public Dashboard(){
+    private static List<DashboardInterface> interfaces;
+    public Dashboard(List<DashboardInterface> interfaces){
         instance = this;
+        this.interfaces = interfaces;
     }
     public static Dashboard getInstance(){
         return instance;
     }
 
-    public static Command dashboardCommand(DashboardInterface subsystem){
-        return subsystem.dashboardCommand();
-    }
+    
 }
