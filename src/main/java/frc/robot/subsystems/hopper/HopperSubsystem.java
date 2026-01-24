abcbca6d2d3e116c48d718e889ed5f9db863e422
1023941311414 <jake.widmann@outlook.com>
2026-01-23 19:52:27 -0500

    Command for hopper

--- src/main/java/frc/robot/subsystems/hopper/HopperSubsystem.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperSubsystem.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems.hopper;
 
+import java.util.function.DoubleSupplier;
+
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class HopperSubsystem extends SubsystemBase {
@@ -10,6 +13,14 @@ public class HopperSubsystem extends SubsystemBase {
 
   public HopperSubsystem(HopperIO io) {
     this.io = io;
+  }
+
+  public Command runVoltageCommand(DoubleSupplier voltage) {
+    return run(() -> io.setHopperVoltage(voltage.getAsDouble())).finallyDo(this::stop);
+  }
+
+  private void stop() {
+    io.setHopperVoltage(0);
   }
 
   @Override
