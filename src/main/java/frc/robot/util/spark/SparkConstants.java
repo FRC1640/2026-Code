391d6731e9c5496344b9afdf70cb57cce85c1467
Kgupta011 <gimonika82@gmail.com>
2026-01-26 23:08:22 -0500

    VAMOS
    finished the current limit for flywheels

--- src/main/java/frc/robot/util/spark/SparkConstants.java
+++ src/main/java/frc/robot/util/spark/SparkConstants.java
@@ -49,6 +49,11 @@ public class SparkConstants {
     return sc;
   }
 
+  public static final SparkConfiguration getFlywheelFlex(int id) {
+    return new SparkConfiguration(id, IdleMode.kCoast, false, 60, 8, 2, StatusFrames.getDefault(),
+        new SparkFlexConfig());
+  }
+
   public static final SparkFlex driveFlex(int id) {
     return SparkConfigurer.configSparkFlex(new SparkConfiguration(id, IdleMode.kBrake, true, 45, 8, 2,
         new StatusFrames(100, 20, (int) (1000 / DriveConstants.odometryFrequency), 500, 500, 500, 500),
