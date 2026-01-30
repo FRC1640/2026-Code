75d64540ad92ecfa1e9e97d1a354756abb939696
Muthu Subhash Muthu <81521335+Bearindigo105@users.noreply.github.com>
2026-01-30 10:21:53 -0500

    Create SwerveDriveSysIdRoutine.java

---
+++ src/main/java/frc/robot/util/sysid/SwerveDriveSysIdRoutine.java
@@ -0,0 +1,37 @@
+package frc.robot.util.sysid;
+
+import static edu.wpi.first.units.Units.*;
+
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.subsystems.module.Module;
+
+public class SwerveDriveSysIdRoutine {
+
+  /**
+   * Creates a new sysid routine with swerve modules
+   *
+   * @param fl
+   *            front left module
+   * @param fr
+   *            front right module
+   * @param bl
+   *            back left module
+   * @param br
+   *            front right module
+   * @param subsystem
+   *            subsystem for requirments
+   * @param config
+   *            config for sysid
+   */
+  public SysIdRoutine createNewRoutine(Module fl, Module fr, Module bl, Module br, SubsystemBase subsystem,
+      SysIdRoutine.Config config) {
+    return new SysIdRoutine(config, new SysIdRoutine.Mechanism((Voltage volts) -> {
+      fl.setDriveVoltage(-volts.in(Volts));
+      fr.setDriveVoltage(-volts.in(Volts));
+      bl.setDriveVoltage(-volts.in(Volts));
+      br.setDriveVoltage(-volts.in(Volts));
+    }, null, subsystem));
+  }
+}
