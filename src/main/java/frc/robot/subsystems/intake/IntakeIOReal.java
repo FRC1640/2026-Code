97c59ec572bfbd52dd017747c7ccae6b0c7ef416
1023941311414 <jake.widmann@outlook.com>
2026-02-04 18:36:36 -0500

    IO cleanup

--- src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
@@ -17,58 +17,61 @@ import frc.robot.util.spark.SparkConfigurer;
 import frc.robot.util.spark.SparkConstants;
 
 public class IntakeIOReal implements IntakeIO {
-  private SparkMax intakeMotor;
-  private SparkMax intakeRoller;
-  private SparkClosedLoopController intakeRollerPID;
-  private PIDController intakePID = RobotPIDConstants.constructPID(RobotPIDConstants.intakeAngleReal);
-  private AbsoluteEncoder intakeEncoder;
-  private AbsoluteEncoder rollerEncoder;
+  private final SparkMax m_intakeMotor;
+  private final AbsoluteEncoder m_intakeEncoder;
+  private final PIDController m_intakePID;
+  private final SparkMax m_rollerMotor;
+  private final AbsoluteEncoder m_rollerEncoder;
+  private final SparkClosedLoopController m_rollerPID;
 
   public IntakeIOReal() {
-    SparkConfiguration config = SparkConstants.getDefaultMax(IntakeConstants.intakeCanID, true);
-    config.getInnerConfig().closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pid(
+    m_intakeMotor = SparkConfigurer.configSparkMax(SparkConstants.getDefaultMax(IntakeConstants.intakeCanID, true));
+    m_intakeEncoder = m_intakeMotor.getAbsoluteEncoder();
+    m_intakePID = RobotPIDConstants.constructPID(RobotPIDConstants.intakeAngleReal);
+    m_intakePID.enableContinuousInput(0, 0.999);
+
+    SparkConfiguration rollerConfig = SparkConstants.getDefaultMax(IntakeConstants.intakeCanID, true);
+    rollerConfig.getInnerConfig().closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pid(
         RobotPIDConstants.intakeRollerReal.kP, RobotPIDConstants.intakeRollerReal.kI,
         RobotPIDConstants.intakeRollerReal.kD, ClosedLoopSlot.kSlot0);
-    intakeMotor = SparkConfigurer.configSparkMax(SparkConstants.getDefaultMax(IntakeConstants.intakeCanID, true));
-    intakeRoller = SparkConfigurer.configSparkMax(SparkConstants.getDefaultMax(IntakeConstants.rollerCanID, true));
-    intakeEncoder = intakeMotor.getAbsoluteEncoder();
-    rollerEncoder = intakeRoller.getAbsoluteEncoder();
-    intakeRollerPID = intakeMotor.getClosedLoopController();
-    intakePID.enableContinuousInput(0, 0.999);
+
+    m_rollerMotor = SparkConfigurer.configSparkMax(rollerConfig);
+    m_rollerEncoder = m_rollerMotor.getAbsoluteEncoder();
+    m_rollerPID = m_intakeMotor.getClosedLoopController();
   }
 
   @Override
   public void setIntakePosition(double pos, IntakeIOInputs inputs) {
     Logger.recordOutput("Subsystems/Intake/Setpoint", pos);
     setIntakeVoltage(IntakeConstants.intakePositionLimits.clampOutput(inputs.intakeEncoderPosition,
-        VoltageLim.clampVoltage(intakePID.calculate(inputs.intakeEncoderPosition, pos))), inputs);
+        VoltageLim.clampVoltage(m_intakePID.calculate(inputs.intakeEncoderPosition, pos))), inputs);
   }
 
   @Override
   public void setIntakeVoltage(double voltage, IntakeIOInputs inputs) {
-    intakeMotor.setVoltage(VoltageLim.clampVoltage(voltage));
+    m_intakeMotor.setVoltage(VoltageLim.clampVoltage(voltage));
   }
 
   @Override
   public void setRollerVelocity(double velocity, IntakeIOInputs inputs) {
-    intakeRollerPID.setSetpoint(velocity, ControlType.kVelocity, ClosedLoopSlot.kSlot0);
+    m_rollerPID.setSetpoint(velocity, ControlType.kVelocity, ClosedLoopSlot.kSlot0);
   }
 
   @Override
   public void setRollerVoltage(double voltage, IntakeIOInputs inputs) {
-    intakeRoller.setVoltage(VoltageLim.clampVoltage(voltage));
+    m_rollerMotor.setVoltage(VoltageLim.clampVoltage(voltage));
   }
 
   @Override
   public void updateInputs(IntakeIOInputs inputs) {
-    inputs.intakeMotorTemperature = intakeMotor.getMotorTemperature(); // degrees celsius
-    inputs.intakeMotorCurrent = intakeMotor.getOutputCurrent(); // amps
-    inputs.intakeMotorVoltage = intakeMotor.getAppliedOutput() * intakeMotor.getBusVoltage(); // volts
-    inputs.intakeEncoderPosition = intakeEncoder.getPosition() * 2 * Math.PI; // radians
-    inputs.intakeEncoderVelocity = intakeEncoder.getVelocity() * 2 * Math.PI / 60; // rad/s
-    inputs.rollerMotorTemperature = intakeRoller.getMotorTemperature(); // degrees celsius
-    inputs.rollerMotorCurrent = intakeRoller.getOutputCurrent(); // amps
-    inputs.rollerMotorVoltage = intakeRoller.getAppliedOutput() * intakeMotor.getBusVoltage(); // volts
-    inputs.rollerEncoderVelocity = rollerEncoder.getVelocity() * 2 * Math.PI / 60; // rad/s
+    inputs.intakeMotorTemperature = m_intakeMotor.getMotorTemperature(); // degrees celsius
+    inputs.intakeMotorCurrent = m_intakeMotor.getOutputCurrent(); // amps
+    inputs.intakeMotorVoltage = m_intakeMotor.getAppliedOutput() * m_intakeMotor.getBusVoltage(); // volts
+    inputs.intakeEncoderPosition = m_intakeEncoder.getPosition() * 2 * Math.PI; // radians
+    inputs.intakeEncoderVelocity = m_intakeEncoder.getVelocity() * 2 * Math.PI / 60; // rad/s
+    inputs.rollerMotorTemperature = m_rollerMotor.getMotorTemperature(); // degrees celsius
+    inputs.rollerMotorCurrent = m_rollerMotor.getOutputCurrent(); // amps
+    inputs.rollerMotorVoltage = m_rollerMotor.getAppliedOutput() * m_intakeMotor.getBusVoltage(); // volts
+    inputs.rollerEncoderVelocity = m_rollerEncoder.getVelocity() * 2 * Math.PI / 60; // rad/s
   }
 }
