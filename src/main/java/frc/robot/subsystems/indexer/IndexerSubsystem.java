8f14cf4862cd4aa83b5dbb9870797aceeafb0c08
1023941311414 <jake.widmann@outlook.com>
2026-02-03 21:26:23 -0500

    Imported command

--- src/main/java/frc/robot/subsystems/indexer/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerSubsystem.java
@@ -4,6 +4,7 @@ import java.util.function.DoubleSupplier;
 
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IndexerSubsystem extends SubsystemBase {
