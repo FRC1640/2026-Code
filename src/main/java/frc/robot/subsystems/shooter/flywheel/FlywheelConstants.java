91e873e303e98fc09a9754345a6e4f2f2539b74b
1023941311414 <jake.widmann@outlook.com>
2026-02-04 19:08:10 -0500

    Flywheel can IDs

--- src/main/java/frc/robot/subsystems/shooter/flywheel/FlywheelConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/flywheel/FlywheelConstants.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems.shooter.flywheel;
 
 public class FlywheelConstants {
 
-  public static final int canId = -1;
-  public static final int followerCanId = -1;
+  public static final int canId = 10;
+  public static final int followerCanId = 11;
 
 }
