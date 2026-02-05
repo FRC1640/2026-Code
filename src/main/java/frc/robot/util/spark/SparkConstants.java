f46ca1cfcd7b1ae6b0f8c89bbbabc78d6cfff76c
Kgupta011 <gimonika82@gmail.com>
2026-02-05 09:40:59 -0500

    spark constants fixed

--- src/main/java/frc/robot/util/spark/SparkConstants.java
+++ src/main/java/frc/robot/util/spark/SparkConstants.java
@@ -53,6 +53,7 @@ public class SparkConstants {
     return new SparkConfiguration(id, IdleMode.kCoast, false, 60, 8, 2, StatusFrames.getDefault(),
         new SparkFlexConfig());
   }
+
   public static final SparkConfiguration getFlywheelFlex(int id, boolean inverted) {
     return new SparkConfiguration(id, IdleMode.kCoast, inverted, 60, 8, 2, StatusFrames.getDefault(),
         new SparkFlexConfig());
@@ -65,33 +66,6 @@ public class SparkConstants {
     return sc;
   }
 
-  public static final SparkConfiguration getDefaultFlex(int id, boolean inverted) {
-    return new SparkConfiguration(id, IdleMode.kCoast, inverted, 45, 8, 2, StatusFrames.getDefault(),
-        new SparkFlexConfig());
-  }
-
-  public static final SparkConfiguration getDefaultFlex(int id, boolean inverted, SparkFlex followerOf) {
-    SparkConfiguration sc = new SparkConfiguration(id, IdleMode.kCoast, inverted, 60, 8, 2,
-        StatusFrames.getDefault(), new SparkFlexConfig());
-    sc.follow(followerOf);
-    return sc;
-  }
-
-  public static final SparkConfiguration getFlywheelFlex(int id) {
-    return new SparkConfiguration(id, IdleMode.kCoast, false, 60, 8, 2, StatusFrames.getDefault(),
-        new SparkFlexConfig());
-  }
-  public static final SparkConfiguration getFlywheelFlex(int id, boolean inverted) {
-    return new SparkConfiguration(id, IdleMode.kCoast, inverted, 60, 8, 2, StatusFrames.getDefault(),
-        new SparkFlexConfig());
-  }
-
-  public static final SparkConfiguration getFlywheelFlex(int id, boolean inverted, SparkFlex followerOf) {
-    SparkConfiguration sc = new SparkConfiguration(id, IdleMode.kCoast, inverted, 60, 8, 2,
-        StatusFrames.getDefault(), new SparkFlexConfig());
-    sc.follow(followerOf);
-    return sc;
-  }
   public static final SparkFlex driveFlex(int id) {
     return SparkConfigurer.configSparkFlex(new SparkConfiguration(id, IdleMode.kBrake, true, 45, 8, 2,
         new StatusFrames(100, 20, (int) (1000 / DriveConstants.odometryFrequency), 500, 500, 500, 500),
