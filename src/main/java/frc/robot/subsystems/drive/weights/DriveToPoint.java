d9f3e840f9a39a11afa47c5f34d17cbce151c4db
smathew01 <mathewshaun25@gmail.com>
2026-02-07 13:39:29 -0500

    weight trigger

--- src/main/java/frc/robot/subsystems/drive/weights/DriveToPoint.java
+++ src/main/java/frc/robot/subsystems/drive/weights/DriveToPoint.java
@@ -9,29 +9,24 @@ import frc.robot.constants.RobotPIDConstants;
 
 public class DriveToPoint implements DriveWeight {
 
-  // TODO Tune
-  Supplier<Pose2d> robotPose, robotTarget;
-  Supplier<Boolean> enabled;
-  PIDController drivePID, rotPID;
+    // TODO Tune
+    Supplier<Pose2d> robotPose, robotTarget;
+    PIDController drivePID, rotPID;
 
-  public DriveToPoint(Supplier<Pose2d> robotPose, Supplier<Pose2d> robotTarget, Supplier<Boolean> enabled) {
-    this.robotPose = robotPose;
-    this.robotTarget = robotTarget;
-    this.enabled = enabled;
-    drivePID = RobotPIDConstants.constructPID(RobotPIDConstants.autoDrivePID);
-    rotPID = RobotPIDConstants.constructPID(RobotPIDConstants.autoTurnPID);
-  }
-
-  @Override
-  public ChassisSpeeds getSpeeds() {
-    if (!enabled.get()) {
-      return new ChassisSpeeds();
-    } else {
-      return new ChassisSpeeds(drivePID.calculate(robotPose.get().getX(), robotTarget.get().getX()),
-          drivePID.calculate(robotPose.get().getY(), robotTarget.get().getY()), rotPID.calculate(
-              robotPose.get().getRotation().getRadians(), robotTarget.get().getRotation().getRadians()));
+    public DriveToPoint(Supplier<Pose2d> robotPose, Supplier<Pose2d> robotTarget) {
+        this.robotPose = robotPose;
+        this.robotTarget = robotTarget;
+        drivePID = RobotPIDConstants.constructPID(RobotPIDConstants.autoDrivePID);
+        rotPID = RobotPIDConstants.constructPID(RobotPIDConstants.autoTurnPID);
     }
 
-  }
+    @Override
+    public ChassisSpeeds getSpeeds() {
+
+        return new ChassisSpeeds(drivePID.calculate(robotPose.get().getX(), robotTarget.get().getX()),
+                drivePID.calculate(robotPose.get().getY(), robotTarget.get().getY()), rotPID.calculate(
+                robotPose.get().getRotation().getRadians(), robotTarget.get().getRotation().getRadians()));
+
+    }
 
 }
