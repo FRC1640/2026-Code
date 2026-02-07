dd3734374aafa99c68b53f19f7566aa006078767
smathew01 <mathewshaun25@gmail.com>
2026-02-07 18:24:31 -0500

    there's a room where the light won't find you

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -1,11 +1,12 @@
 package frc.robot.subsystems.intake;
 
 import frc.robot.util.limits.Limits;
+import frc.robot.util.robotswitcher.SwitchableCANID;
 
 public class IntakeConstants {
   // TODO: change canids
-  public static final int intakeCanID = -1;
-  public static final int rollerCanID = -1;
+  public static final int intakeCanID = SwitchableCANID.of(15).get();
+  public static final int rollerCanID = SwitchableCANID.of(16).get();;
   public static final double gearRatio = 1;
   public static final double rollerGearRatio = 1;
 
--- src/main/java/frc/robot/subsystems/spindexer/SpindexerConstants.java
+++ src/main/java/frc/robot/subsystems/spindexer/SpindexerConstants.java
@@ -1,7 +1,11 @@
 package frc.robot.subsystems.spindexer;
 
+import frc.robot.util.robotswitcher.SwitchableCANID;
+
 public class SpindexerConstants {
-  public static int indexerSparkCanId = -1; // TODO: set id and inverted and set to actual values
+
+  public static int indexerSparkCanId = SwitchableCANID.of(14).get();; // TODO: set id and inverted and set to actual
+  // values
   public static boolean indexerSparkInverted = false;
 
   public static double indexerGearRatioSim = 1;
