f6f3fceea97179d19213a66d1b156773913aedd1
tgschmale <122210387+tgschmale@users.noreply.github.com>
2026-02-07 20:08:20 -0500

    Feature/gyro bump detect (#91)
    * added a periodic method to calculate the max angle... but the angle is always 1.535. debugging needed
    
    * fixed the math. everything seems to work!
    
    * fix a merge issue
    
    * remove some testing in robot container
    
    * add a comment
    
    * apply spotless
    
    * changed name of func from periodic to updateAngle
    
    * testing features
    
    * minor changes, some testing stuff
    
    * remove a binding. also, last commit remozed turret from frank
    
    * Spotless applied
    
    ---------
    
    Co-authored-by: 1023941311414 <jake.widmann@outlook.com>

---
+++ src/main/java/frc/robot/sensors/gyro/BumpDetectorPeriodic.java
@@ -0,0 +1,80 @@
+package frc.robot.sensors.gyro;
+
+import org.ejml.simple.SimpleMatrix;
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.geometry.Translation3d;
+import frc.robot.util.periodic.PeriodicBase;
+
+public class BumpDetectorPeriodic extends PeriodicBase {
+  private double[] angle;
+  private Gyro gyro;
+  private final double cutoff;
+  private final int len;
+
+  public BumpDetectorPeriodic(Gyro gyro, int len, double cutoff) {
+    this.gyro = gyro;
+    angle = new double[len];
+    this.len = len;
+    this.cutoff = cutoff;
+
+  }
+
+  @Override
+  public void periodic() {
+    updateAngle(gyro.getPitch().getRadians(), gyro.getRoll().getRadians());
+    Logger.recordOutput("BumpDetection/bumpDetected", bumpDetected());
+  }
+
+  private void updateAngle(double pitch, double roll) {
+    for (int i = len - 1; i > 0; i--) {
+      angle[i] = angle[i - 1];
+    }
+    // custom formating
+    Translation3d pitchVector = new Translation3d(
+        Math.abs(Math.cos(pitch)), 
+        0,
+        Math.abs(Math.sin(pitch)));
+    Translation3d rollVector = new Translation3d(
+        0,
+        Math.abs(Math.cos(roll)),
+        Math.abs(Math.sin(roll)));
+    // spotless formating
+    angle[0] = Math.abs(
+        Math.acos((pitchVector.cross(rollVector)).dot(new Vector<>(new SimpleMatrix(3, 1, true, 0, 0, 1)))));
+    // the above math is shown in this graph. it is poorly done, so if you need to
+    // rework math, please find me, or try to make sense of it. best of luck.
+    // https://www.desmos.com/3d/d6gpdlk7zj
+  }
+
+  public boolean bumpDetected() {
+    double max = 0;
+    for (int i = 0; i < len; i++) {
+      max = (angle[i] > max) ? angle[i] : max;
+    }
+    return (max > cutoff);
+  }
+
+  private double getDouble() {
+    double max = 0;
+    for (int i = 0; i < len; i++) {
+      max = (angle[i] > max) ? angle[i] : max;
+    }
+    return (max);
+  }
+
+  public void test() {
+    double[] testArray = {0, Math.PI / 6, Math.PI / 3, Math.PI / 2, 2 * Math.PI / 3, 5 * Math.PI / 6, Math.PI, 0, 0,
+        0};
+    for (double i : testArray) {
+      System.out.println("\n" + i + ":    ");
+      for (double j : testArray) {
+        updateAngle(i, j);
+        // System.out.print(j+": "+getDouble()+" ");
+        System.out.print(String.format("%.2f", j) + ": " + String.format("%.2f", getDouble()) + "      ");
+      }
+    }
+  }
+
+}
