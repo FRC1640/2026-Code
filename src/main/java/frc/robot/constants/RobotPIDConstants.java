462f315318e0cc46e0858a5f9414ef0c14b24553
Shaun Mathew <143555953+SpeedSlicer@users.noreply.github.com>
2026-02-08 13:38:59 -0500

    Feature/trench lock (#115)
    * burn dubhash at the bake
    
    * We should do Mr/Mrs. 1640
    
    * cheese (blame Shaun)
    
    * Bam Weights are weighting dimensionally
    
    ---------
    
    Co-authored-by: 1023941311414 <151398282+1023941311414@users.noreply.github.com>
    Co-authored-by: Muthu Subhash Muthu <81521335+Bearindigo105@users.noreply.github.com>
    Co-authored-by: Muthu Subhash Muthu <m.muthusubhash@gmail.com>

--- src/main/java/frc/robot/constants/RobotPIDConstants.java
+++ src/main/java/frc/robot/constants/RobotPIDConstants.java
@@ -1,8 +1,6 @@
 package frc.robot.constants;
 
 import com.pathplanner.lib.config.PIDConstants;
-import frc.robot.util.FeedForwardConstants;
-
 import com.revrobotics.spark.config.MAXMotionConfig;
 import com.revrobotics.spark.config.MAXMotionConfig.MAXMotionPositionMode;
 
@@ -11,6 +9,7 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import frc.robot.util.FeedForwardConstants;
 import frc.robot.util.logging.PID.PIDStorage;
 import frc.robot.util.logging.PPID.PPIDStorage;
 
@@ -54,8 +53,8 @@ public class RobotPIDConstants {
   public static final FeedForwardConstants flywheelVelocityFF = new FeedForwardConstants(0, 0, 0);
 
   // DriveToPoint
-  public static final PIDConstants autoDrivePID = new PIDConstants(1, 0, 0);
-  public static final PIDConstants autoTurnPID = new PIDConstants(1, 0, 0);
+  public static final PIDConstants autoDrivePID = new PIDConstants(2, 0, 0);
+  public static final PIDConstants autoTurnPID = new PIDConstants(2, 0, 0);
 
   /*-----------------------
   * CONSTRUCTION HELPERS |
