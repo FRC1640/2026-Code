1fb677abcb45b318244116102ceffa5b77f830aa
1023941311414 <jake.widmann@outlook.com>
2026-02-12 12:25:43 -0500

    Deflector cleanup

--- src/main/java/frc/robot/subsystems/shooter/deflector/DeflectorSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/deflector/DeflectorSubsystem.java
@@ -25,27 +25,33 @@ public class DeflectorSubsystem extends SubsystemPlatform {
     this.io = io;
   }
 
+  /*
+   * Commands
+   */
+
+  public Command setAngleCommand(DoubleSupplier angle) {
+    return run(() -> io.setAngle(angle.getAsDouble()));
+  }
+
+  public Command setAngleCommand(Supplier<TurretSetpoint> setpoint) {
+    return run(() -> io.setAngle(setpoint.get()));
+  }
+
   public Command runVoltageCommand(DoubleSupplier voltage) {
     return run(() -> io.setVoltage(voltage.getAsDouble())).finallyDo(this::stop);
   }
 
-  private void stop() {
-    io.setVoltage(0);
+  public Command stopCommand() {
+    return runOnce(this::stop);
   }
 
   @Override
   public Command dashboardCommand(DoubleSupplier leftJoystickValue, DoubleSupplier rightJoystickValue) {
     return runVoltageCommand(() -> leftJoystickValue.getAsDouble() * -8);
   }
-  /*
-   * Commands
-   */
-  public Command runHoodToAngle(DoubleSupplier angle) {
-    return run(() -> io.setAngle(angle.getAsDouble()));
-  }
 
-  public Command runHoodToSetpoint(Supplier<TurretSetpoint> setpoint) {
-    return run(() -> io.setAngle(setpoint.get()));
+  private void stop() {
+    io.setVoltage(0);
   }
 
   @Override
