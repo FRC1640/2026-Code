fb09ab0e68b51a64d348a1640dfdcb2c43368cca
1023941311414 <jake.widmann@outlook.com>
2026-02-18 18:35:13 -0500

    IO

--- src/main/java/frc/robot/subsystems/climber/ClimberIO.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIO.java
@@ -1,5 +1,23 @@
 package frc.robot.subsystems.climber;
 
-public class ClimberIO {
-  
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ClimberIO extends AutoCloseable {
+  @AutoLog
+  public class ClimberIOInputs {
+    public double encoderPosition;
+    public double encoderVelocity;
+    public double motorCurrent;
+    public double motorVoltage;
+    public double motorTemperature;
+  }
+
+  public default void setPosition(double position) {}
+
+  public default void setVoltage(double voltage) {}
+
+  public default void updateInputs(ClimberIOInputs inputs) {}
+
+  @Override
+  public default void close() {}
 }
--- src/main/java/frc/robot/subsystems/climber/ClimberIOReal.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOReal.java
@@ -1,5 +1,13 @@
 package frc.robot.subsystems.climber;
 
-public class ClimberIOReal {
-  
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.spark.SparkFlex;
+
+public class ClimberIOReal implements ClimberIO {
+  private final SparkFlex m_motor;
+  private final AbsoluteEncoder m_encoder;
+
+  public ClimberIOReal() {
+    
+  }
 }
