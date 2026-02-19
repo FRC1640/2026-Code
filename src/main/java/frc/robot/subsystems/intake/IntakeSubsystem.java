d560783ed224c65c8b24e73f872453eee106c4dc
tgschmale <tyguysuper@gmail.com>
2026-02-18 20:13:03 -0500

    Jake. you're welcome.

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -45,7 +45,7 @@ public class IntakeSubsystem extends SubsystemPlatform {
     return setPositionCommand(IntakeConstants.upPosition);
   }
 
-  public Command ossilateIntakeCommand(double pos, double amp, double freq) {
+  public Command oscillateIntakeCommand(double pos, double amp, double freq) {
     Timer t  = new Timer();
     return new InstantCommand(() -> {t.start();}).andThen(setPositionCommand(() -> pos + amp*Math.sin(t.get()*freq))).finallyDo(()->{t.stop(); t.reset();});
   }
