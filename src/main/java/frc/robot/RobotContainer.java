234d4fa4897336c7b0cb3195d18ad5231d8f7176
1023941311414 <jake.widmann@outlook.com>
2026-02-21 12:59:57 -0500

    Removed test bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -158,8 +158,6 @@ public class RobotContainer {
                 lockToPointWeight.getTargetPoint().getY(), lockToPointWeight.getRobotPose().getY()))
             && (DistanceManager.inRange(LockToPoint.activeDistanceY,
                 lockToPointWeight.getTargetPoint().getX(), lockToPointWeight.getRobotPose().getX())));
-    operatorController.a().onTrue(climberSubsystem.setPositionCommand(1));
-    operatorController.b().onTrue(climberSubsystem.setPositionCommand(0));
   }
 
   private void generateTriggers() {
