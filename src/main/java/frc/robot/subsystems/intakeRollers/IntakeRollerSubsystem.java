53a4ff4025cfbc0fd1b5aaccb6cc26c3d87e24d9
1023941311414 <jake.widmann@outlook.com>
2026-02-21 18:55:07 -0500

    Working intake command

--- src/main/java/frc/robot/subsystems/intakeRollers/IntakeRollerSubsystem.java
+++ src/main/java/frc/robot/subsystems/intakeRollers/IntakeRollerSubsystem.java
@@ -5,6 +5,7 @@ import java.util.function.DoubleSupplier;
 import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import frc.robot.Robot;
 import frc.robot.constants.RobotConstants;
 import frc.robot.constants.RobotConstants.RobotTypes;
