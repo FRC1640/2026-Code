3d1714326ba5f973264120c811640d9c891ae4e3
Kgupta011 <gimonika82@gmail.com>
2026-02-21 20:14:57 -0500

    wt4

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -37,7 +37,8 @@ public class RobotConstants {
     public static final SubsystemInfo climberSubsystem = new SubsystemInfo("Climber");
     // ROBOTS
     public static final RobotType duex26 = new RobotType("Duex26", driveSubsystem, kickerSubsystem,
-        spindexerSubsystem, hoodSubsystem, shooterSubsystem, turretSubsystem, intakeSubsystem);
+        spindexerSubsystem, hoodSubsystem, shooterSubsystem, turretSubsystem, intakeSubsystem,
+        intakeRollerSubsystem);
 
     public static final RobotType frank25 = new RobotType("Frank25", driveSubsystem)
         .addAprilTagCamera(CameraSettings.frankOdometryCamera);
