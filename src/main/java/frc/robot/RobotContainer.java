8583adefc187b82b176016fb2bf30d69f2d0fbfa
tgschmale <122210387+tgschmale@users.noreply.github.com>
2026-02-22 16:51:27 -0500

    did everything (#164)
    * did everything
    
    * blamo

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,6 +21,7 @@ import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -168,7 +169,7 @@ public class RobotContainer {
 
   private void generateTriggers() {
     new Trigger(() -> bumpDetector.bumpDetected())
-        .whileTrue(new InstantCommand(() -> RobotOdometry.instance.distrustDrive("Main")));
+        .whileTrue(new RunCommand(() -> RobotOdometry.instance.distrustDrive("Main")));
   }
 
   private void configureDefaultCommands() {
@@ -181,6 +182,9 @@ public class RobotContainer {
   }
 
   private void generateNamedCommands() {
+    NamedCommands.registerCommand("DistrustOdometry", new InstantCommand(() -> {
+      RobotOdometry.instance.distrustDrive("Main");
+    }));
     NamedCommands.registerCommand("EnableAprilTags",
         new InstantCommand(() -> RobotOdometry.instance.setAutoApriltags(true)));
     NamedCommands.registerCommand("DisableAprilTags",
