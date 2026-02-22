eaf666c0c573e0ebafaf803e6af089ca18b9a4bc
1023941311414 <jake.widmann@outlook.com>
2026-02-22 16:21:32 -0500

    Oscillate intake and run rollers backwards when shooting? Needs testing

--- src/main/java/frc/robot/RobotCommands.java
+++ src/main/java/frc/robot/RobotCommands.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
 import frc.robot.subsystems.ShotControl;
 import frc.robot.subsystems.drive.DriveSubsystem;
 import frc.robot.subsystems.hood.HoodSubsystem;
@@ -56,11 +57,18 @@ public class RobotCommands {
   // SHOT CONTROL COMMANDS
   public Command shootCommand() {
     ShotControl shotControl = ShotControl.getInstance();
-    return shooterSubsystem.shootCommand()
-        .alongWith(/* hoodSubsystem.runHoodToSetpointCommand(), */ kickerSubsystem.runCommand(),
-            new InstantCommand(() -> shotControl.setShooting(true)),
-            new WaitUntilCommand(() -> shooterSubsystem.isAtSetpoint() /* && hoodSubsystem.isAtSetpoint() */
-                && kickerSubsystem.isAtSetpoint()).andThen(spindexerSubsystem.runCommand()))
+    return shooterSubsystem.shootCommand().alongWith(
+        /* hoodSubsystem.runHoodToSetpointCommand(), */ kickerSubsystem.runCommand(),
+        new InstantCommand(() -> shotControl.setShooting(true)),
+        new WaitUntilCommand(() -> shooterSubsystem.isAtSetpoint() /* && hoodSubsystem.isAtSetpoint() */
+            && kickerSubsystem.isAtSetpoint())
+                .andThen(spindexerSubsystem.runCommand().alongWith(
+                    new InstantCommand(() -> CommandScheduler.getInstance()
+                        .schedule(intakeSubsystem.oscillateIntakeCommand(30, 10, 2)
+                            .withInterruptBehavior(InterruptionBehavior.kCancelSelf))),
+                    new InstantCommand(() -> CommandScheduler.getInstance()
+                        .schedule(intakeRollerSubsystem.runVoltageCommand(() -> 8)
+                            .withInterruptBehavior(InterruptionBehavior.kCancelSelf))))))
         .finallyDo(() -> shotControl.setShooting(false));
   }
 
--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -43,8 +43,9 @@ public class IntakeSubsystem extends SubsystemPlatform {
     return setPositionCommand(IntakeConstants.stowedPositionRadians);
   }
 
-  public Command oscillateIntakeCommand(double pos, double amp, double freq) {
-    return new TimedCommand((t) -> io.setPosition(pos + amp * Math.sin(t * freq))).finallyDo(this::stop);
+  public Command oscillateIntakeCommand(double angleDegrees, double amp, double freq) {
+    return new TimedCommand((t) -> io.setPosition(angleDegrees + amp * Math.sin(t * 2 * Math.PI * freq)))
+        .finallyDo(this::stop);
   }
 
   @Override
