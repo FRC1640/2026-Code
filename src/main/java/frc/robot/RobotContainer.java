24f9d92c8f2ff5a9fcfeb285e0b07edf30ed38cc
Shaun Mathew <143555953+SpeedSlicer@users.noreply.github.com>
2026-02-21 19:53:33 -0500

    mechs (#162)
    * mechs
    
    * SPOTLESS
    
    * Whitespace
    
    * Removed test bindings and converted intake angle to mechanism angle
    
    * Removed last intake position
    
    * Made motor dashboard private
    
    ---------
    
    Co-authored-by: 1023941311414 <jake.widmann@outlook.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import edu.wpi.first.math.geometry.Pose3d;
 import frc.robot.constants.RobotConstants;
 import frc.robot.sensors.apriltag.AprilTagVisionIO;
 import frc.robot.sensors.apriltag.CameraConstant;
+
 import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.math.geometry.Pose2d;
@@ -46,13 +47,13 @@ import frc.robot.util.helpers.AllianceManager;
 import frc.robot.util.helpers.DistanceManager;
 import frc.robot.util.logging.AlertsManager;
 import frc.robot.util.logging.PeriodicLogging;
-import frc.robot.util.motorDashboard.Dashboard;
+import frc.robot.util.motorDashboard.MotorDashboard;
 import frc.robot.util.networktables.AutonChooser;
 import frc.robot.util.sysid.SysIdChooser;
 
 public class RobotContainer {
-  // controllers
 
+  // controllers
   private CommandXboxController driveController;
   private CommandXboxController operatorController;
 
@@ -76,6 +77,7 @@ public class RobotContainer {
   private JoystickDriveWeight joystickDriveWeight;
   private DriveToPoint driveToPointWeight;
   private LockToPoint lockToPointWeight;
+
   // dashboards
   private SysIdChooser sysIdChooser;
   private AutonChooser autonChooser;
@@ -86,6 +88,7 @@ public class RobotContainer {
   private RobotCommands robotCommands;
   private AlertsManager alertsManager;
   private BumpDetectorPeriodic bumpDetector;
+  private MotorDashboard dashboard;
 
   public RobotContainer() {
     // custom formatting
@@ -181,8 +184,8 @@ public class RobotContainer {
   }
 
   public void initializeDashboard() {
-    new Dashboard(kickerSubsystem, spindexerSubsystem, hoodSubsystem, shooterSubsystem, turretSubsystem,
-        intakeSubsystem, intakeRollerSubsystem);
+    dashboard = new MotorDashboard(kickerSubsystem, spindexerSubsystem, hoodSubsystem, shooterSubsystem,
+        turretSubsystem, intakeSubsystem, intakeRollerSubsystem);
   }
 
   private void loadResources() {
---
+++ src/main/java/frc/robot/util/math/MathExtras.java
@@ -0,0 +1,9 @@
+package frc.robot.util.math;
+
+public class MathExtras {
+
+  public static double map(double value, double inMin, double inMax, double outMin, double outMax) {
+
+    return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+  }
+}
