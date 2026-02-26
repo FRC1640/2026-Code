74c59908a2f1a0e52c8002d2a41f85ca5440b16d
smathew01 <mathewshaun25@gmail.com>
2026-02-25 20:58:09 -0500

    Revert "trying to figure out max motion, vol 1"
    This reverts commit 581784b469adc4e9051676eccce1327b058d0c6f.

--- src/main/java/frc/robot/subsystems/turret/TurretIOReal.java
+++ src/main/java/frc/robot/subsystems/turret/TurretIOReal.java
@@ -17,62 +17,61 @@ import frc.robot.util.spark.SparkConfigurer;
 import frc.robot.util.spark.SparkConstants;
 
 public class TurretIOReal implements TurretIO {
+  private final SparkMax m_motor;
+  private final SparkAnalogSensor m_encoder;
+  private final RelativeEncoder m_relativeEncoder;
+  private final PIDController m_positionController;
+  private final SimpleMotorFeedforward m_feedforwardController;
 
-    private final SparkMax m_motor;
-    private final SparkAnalogSensor m_encoder;
-    private final RelativeEncoder m_relativeEncoder;
-    private final ProfiledPIDController m_positionController;
-    private final SimpleMotorFeedforward m_feedforwardController;
+  public TurretIOReal() {
+    m_motor = SparkConfigurer.configSparkMax(TurretConstants.canId, SparkConstants.turretConfig);
+    m_encoder = m_motor.getAnalog();
+    m_positionController = RobotPIDConstants.constructPID(RobotPIDConstants.turretAnglePidReal);
+    m_feedforwardController = RobotPIDConstants.constructFFSimpleMotor(RobotPIDConstants.turretAngleFF);
+    m_relativeEncoder = m_motor.getEncoder();
+  }
 
-    public TurretIOReal() {
-        m_motor = SparkConfigurer.configSparkMax(TurretConstants.canId, SparkConstants.turretConfig);
-        m_encoder = m_motor.getAnalog();
-        m_positionController = RobotPIDConstants.constructProfiledPIDController(RobotPIDConstants.turretAnglePidReal, RobotPIDConstants.turretConstraints);
-        m_feedforwardController = RobotPIDConstants.constructFFSimpleMotor(RobotPIDConstants.turretAngleFF);
-        m_relativeEncoder = m_motor.getEncoder();
-    }
+  @Override
+  public void setTurretState(double angle, double angularVelocity) {
+    double clampedAngle = TurretConstants.turretAngleLimits.clampPosition(angle);
+    double voltage = m_positionController.calculate(getTurretPosition(), clampedAngle)
+        + m_feedforwardController.calculate(angularVelocity);
+    setVoltage(voltage);
+  }
 
-    @Override
-    public void setTurretState(double angle, double angularVelocity) {
-        double clampedAngle = TurretConstants.turretAngleLimits.clampPosition(angle);
-        double voltage = m_positionController.calculate(getTurretPosition(), clampedAngle)
-                + m_feedforwardController.calculate(angularVelocity);
-        setVoltage(voltage);
-    }
+  @Override
+  public void setVoltage(double voltage) {
+    double voltageClamped = MotorLim.clampVoltage(voltage);
+    voltageClamped = TurretConstants.turretAngleLimits.clampOutput(getTurretPosition(), voltageClamped);
+    m_motor.setVoltage(voltageClamped);
+  }
 
-    @Override
-    public void setVoltage(double voltage) {
-        double voltageClamped = MotorLim.clampVoltage(voltage);
-        voltageClamped = TurretConstants.turretAngleLimits.clampOutput(getTurretPosition(), voltageClamped);
-        m_motor.setVoltage(voltageClamped);
-    }
+  @Override
+  public void updateInputs(TurretIOInputs inputs) {
+    inputs.angleRadians = getTurretPosition();
+    inputs.angularVelocityRadPerSec = getTurretVelocity();
+    inputs.angleDegrees = inputs.angleRadians * 180 / Math.PI;
+    inputs.angularVelocityDegreesPerSec = inputs.angularVelocityRadPerSec * 180 / Math.PI;
+    inputs.motorCurrent = m_motor.getOutputCurrent();
+    inputs.motorVoltage = m_motor.getBusVoltage() * m_motor.getAppliedOutput();
+    inputs.motorTemperatureCelsius = m_motor.getMotorTemperature();
 
-    @Override
-    public void updateInputs(TurretIOInputs inputs) {
-        inputs.angleRadians = getTurretPosition();
-        inputs.angularVelocityRadPerSec = getTurretVelocity();
-        inputs.angleDegrees = inputs.angleRadians * 180 / Math.PI;
-        inputs.angularVelocityDegreesPerSec = inputs.angularVelocityRadPerSec * 180 / Math.PI;
-        inputs.motorCurrent = m_motor.getOutputCurrent();
-        inputs.motorVoltage = m_motor.getBusVoltage() * m_motor.getAppliedOutput();
-        inputs.motorTemperatureCelsius = m_motor.getMotorTemperature();
+    Logger.recordOutput("Subsystems/Turret/encoderPositionRaw", m_encoder.getPosition());
+    Logger.recordOutput("Subsystems/Turret/encoderVelocityRaw", m_encoder.getVelocity());
+  }
 
-        Logger.recordOutput("Subsystems/Turret/encoderPositionRaw", m_encoder.getPosition());
-        Logger.recordOutput("Subsystems/Turret/encoderVelocityRaw", m_encoder.getVelocity());
-    }
+  private double getTurretPosition() {
+    return (m_encoder.getPosition() - TurretConstants.pot0degVoltage)
+        / (TurretConstants.pot90degVoltage - TurretConstants.pot0degVoltage) * Math.PI / 2;
+  }
 
-    private double getTurretPosition() {
-        return (m_encoder.getPosition() - TurretConstants.pot0degVoltage)
-                / (TurretConstants.pot90degVoltage - TurretConstants.pot0degVoltage) * Math.PI / 2;
-    }
+  private double getTurretVelocity() {
+    return m_encoder.getVelocity() / (TurretConstants.pot90degVoltage - TurretConstants.pot0degVoltage) * Math.PI
+        / 2;
+  }
 
-    private double getTurretVelocity() {
-        return m_encoder.getVelocity() / (TurretConstants.pot90degVoltage - TurretConstants.pot0degVoltage) * Math.PI
-                / 2;
-    }
-
-    public boolean isSensorDisconnected() {
-        return Math.abs(m_relativeEncoder.getVelocity()) > disconnectMinMotorVelocity
-                && Math.abs(m_encoder.getVelocity()) < disconnectMinPotVelocity;
-    }
+  public boolean isSensorDisconnected() {
+    return Math.abs(m_relativeEncoder.getVelocity()) > disconnectMinMotorVelocity
+        && Math.abs(m_encoder.getVelocity()) < disconnectMinPotVelocity;
+  }
 }
--- src/main/java/frc/robot/util/spark/SparkConstants.java
+++ src/main/java/frc/robot/util/spark/SparkConstants.java
@@ -5,142 +5,120 @@ import com.revrobotics.spark.FeedbackSensor;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.LimitSwitchConfig;
-import com.revrobotics.spark.config.MAXMotionConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-
-import edu.wpi.first.math.util.Units;
-
 import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import frc.robot.subsystems.drive.DriveConstants;
-import frc.robot.subsystems.hood.HoodConstants;
 import frc.robot.subsystems.shooter.ShooterConstants;
 import frc.robot.subsystems.spindexer.SpindexerConstants;
 
 public class SparkConstants {
+  public static final SparkFlexConfig shooterLeaderConfig;
+  public static final SparkFlexConfig shooterFollowerConfig;
+  public static final SparkMaxConfig hoodConfig;
+  public static final SparkMaxConfig spindexerConfig;
+  public static final SparkMaxConfig intakeConfig;
+  public static final SparkMaxConfig intakeRollerConfig;
+  public static final SparkMaxConfig kickerConfig;
+  public static final SparkMaxConfig turretConfig;
+  public static final SparkFlexConfig climberConfig;
 
-    public static final SparkFlexConfig shooterLeaderConfig;
-    public static final SparkFlexConfig shooterFollowerConfig;
-    public static final SparkMaxConfig hoodConfig;
-    public static final SparkMaxConfig spindexerConfig;
-    public static final SparkMaxConfig intakeConfig;
-    public static final SparkMaxConfig intakeRollerConfig;
-    public static final SparkMaxConfig kickerConfig;
-    public static final SparkMaxConfig turretConfig;
-    public static final SparkFlexConfig climberConfig;
+  static {
+    shooterLeaderConfig = getDefaultFlexConfig();
+    shooterLeaderConfig.smartCurrentLimit(80, 80).closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+        .pid(0.0004, 0, 0.003, ClosedLoopSlot.kSlot0).pid(0.0006, 0, 0, ClosedLoopSlot.kSlot1)
+        .pid(0.0001, 0, 0, ClosedLoopSlot.kSlot2).pid(0.0001, 0, 0, ClosedLoopSlot.kSlot3).feedForward
+            .kV(0.002, ClosedLoopSlot.kSlot0).kA(0.0001, ClosedLoopSlot.kSlot0)
+            .kV(0.002, ClosedLoopSlot.kSlot1).kA(0.002, ClosedLoopSlot.kSlot1)
+            .kV(0.0017, ClosedLoopSlot.kSlot2).kV(0.0019, ClosedLoopSlot.kSlot3);
+    shooterLeaderConfig.closedLoop.maxMotion.maxAcceleration(4000, ClosedLoopSlot.kSlot0).maxAcceleration(4000,
+        ClosedLoopSlot.kSlot1);
+    shooterLeaderConfig.smartCurrentLimit(80, 80);
+    shooterFollowerConfig = (SparkFlexConfig) getDefaultFlexConfig().follow(ShooterConstants.canId, true);
+    hoodConfig = getDefaultMaxConfig();
+    hoodConfig.closedLoop.feedbackSensor(FeedbackSensor.kAbsoluteEncoder).pid(2.5, 0, 0, ClosedLoopSlot.kSlot0);
+    hoodConfig.inverted(true).absoluteEncoder.inverted(true);
+    spindexerConfig = (SparkMaxConfig) getDefaultMaxConfig().inverted(SpindexerConstants.indexerSparkInverted);
+    intakeConfig = (SparkMaxConfig) new SparkMaxConfig().idleMode(IdleMode.kBrake).inverted(false);
+    intakeRollerConfig = getDefaultMaxConfig();
+    kickerConfig = getDefaultMaxConfig();
+    turretConfig = getDefaultMaxConfig();
+    turretConfig.inverted(true);
+    climberConfig = getDefaultFlexConfig();
+    climberConfig.idleMode(IdleMode.kBrake).closedLoop.feedbackSensor(FeedbackSensor.kAbsoluteEncoder).pid(1, 0, 0,
+        ClosedLoopSlot.kSlot0);
+  }
 
-    static {
-        shooterLeaderConfig = getDefaultFlexConfig();
-        shooterLeaderConfig.smartCurrentLimit(80, 80).closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-                .pid(0.0004, 0, 0.003, ClosedLoopSlot.kSlot0).pid(0.0006, 0, 0, ClosedLoopSlot.kSlot1)
-                .pid(0.0001, 0, 0, ClosedLoopSlot.kSlot2).pid(0.0001, 0, 0, ClosedLoopSlot.kSlot3).feedForward
-                .kV(0.002, ClosedLoopSlot.kSlot0).kA(0.0001, ClosedLoopSlot.kSlot0)
-                .kV(0.002, ClosedLoopSlot.kSlot1).kA(0.002, ClosedLoopSlot.kSlot1)
-                .kV(0.0017, ClosedLoopSlot.kSlot2).kV(0.0019, ClosedLoopSlot.kSlot3);
-        shooterLeaderConfig.closedLoop.maxMotion.maxAcceleration(4000, ClosedLoopSlot.kSlot0).maxAcceleration(4000,
-                ClosedLoopSlot.kSlot1);
-        shooterLeaderConfig.smartCurrentLimit(80, 80);
-        shooterFollowerConfig = (SparkFlexConfig) getDefaultFlexConfig().follow(ShooterConstants.canId, true);
-        
-        // LimitSwitchConfig hoodLimitSwitches = new LimitSwitchConfig().forwardLimitSwitchType(LimitSwitchConfig.Type.kNormallyOpen).forwardLimitSwitchTriggerBehavior(LimitSwitchConfig.Behavior.kStopMovingMotorAndSetPosition);
-        hoodConfig = getDefaultMaxConfig();
-        hoodConfig.closedLoop.feedbackSensor(FeedbackSensor.kAbsoluteEncoder).pid(1, 0, 0, ClosedLoopSlot.kSlot0);
-        hoodConfig.closedLoop.apply(
-          new MAXMotionConfig()
-          .cruiseVelocity(Units.radiansPerSecondToRotationsPerMinute(HoodConstants.maxVelocityRadiansPerSecond))
-          .allowedProfileError(Units.degreesToRotations(10))
-          .maxAcceleration(Units.radiansPerSecondToRotationsPerMinute(HoodConstants.maxAccelerationRadiansPerSecond))
-          .positionMode(MAXMotionConfig.MAXMotionPositionMode.kMAXMotionTrapezoidal)
-        );
-        hoodConfig.inverted(true).absoluteEncoder.inverted(true);
-        
-        spindexerConfig = (SparkMaxConfig) getDefaultMaxConfig().inverted(SpindexerConstants.indexerSparkInverted);
-        
-        intakeConfig = (SparkMaxConfig) new SparkMaxConfig().idleMode(IdleMode.kBrake).inverted(false);
-        intakeRollerConfig = getDefaultMaxConfig();
-        
-        kickerConfig = getDefaultMaxConfig();
-        
-        turretConfig = getDefaultMaxConfig();
-        turretConfig.inverted(true);
-        
-        climberConfig = getDefaultFlexConfig();
-        climberConfig.idleMode(IdleMode.kBrake).closedLoop.feedbackSensor(FeedbackSensor.kAbsoluteEncoder).pid(1, 0, 0,
-                ClosedLoopSlot.kSlot0);
-    }
+  private static final SparkMaxConfig getDefaultMaxConfig() {
+    SparkMaxConfig config = new SparkMaxConfig();
+    config.idleMode(IdleMode.kCoast).inverted(false);
+    return config;
+  }
 
-    private static final SparkMaxConfig getDefaultMaxConfig() {
-        SparkMaxConfig config = new SparkMaxConfig();
-        config.idleMode(IdleMode.kCoast).inverted(false);
-        return config;
-    }
+  private static final SparkFlexConfig getDefaultFlexConfig() {
+    SparkFlexConfig config = new SparkFlexConfig();
+    config.idleMode(IdleMode.kCoast).inverted(false);
+    return config;
+  }
 
-    private static final SparkFlexConfig getDefaultFlexConfig() {
-        SparkFlexConfig config = new SparkFlexConfig();
-        config.idleMode(IdleMode.kCoast).inverted(false);
-        return config;
-    }
+  public static final SparkConfiguration getDefaultMax(int id, boolean inverted) {
+    return new SparkConfiguration(id, IdleMode.kCoast, inverted, 60, 8, 2, StatusFrames.getDefault(),
+        new SparkMaxConfig());
+  }
 
-    public static final SparkConfiguration getDefaultMax(int id, boolean inverted) {
-        return new SparkConfiguration(id, IdleMode.kCoast, inverted, 60, 8, 2, StatusFrames.getDefault(),
-                new SparkMaxConfig());
-    }
+  public static final SparkConfiguration getDefaultMax(int id, boolean inverted, boolean useAbsolute) {
+    return new SparkConfiguration(id, IdleMode.kCoast, inverted, 60, 8, 2, StatusFrames.getDefault(), null,
+        new LimitSwitchConfig(), new SparkMaxConfig(), useAbsolute);
+  }
 
-    public static final SparkConfiguration getDefaultMax(int id, boolean inverted, boolean useAbsolute) {
-        return new SparkConfiguration(id, IdleMode.kCoast, inverted, 60, 8, 2, StatusFrames.getDefault(), null,
-                new LimitSwitchConfig(), new SparkMaxConfig(), useAbsolute);
-    }
+  public static final SparkConfiguration getDefaultMax(int id, boolean inverted, IdleMode brakeMode) {
+    return new SparkConfiguration(id, brakeMode, inverted, 60, 8, 2, StatusFrames.getDefault(),
+        new SparkMaxConfig());
+  }
 
-    public static final SparkConfiguration getDefaultMax(int id, boolean inverted, IdleMode brakeMode) {
-        return new SparkConfiguration(id, brakeMode, inverted, 60, 8, 2, StatusFrames.getDefault(),
-                new SparkMaxConfig());
-    }
+  public static final SparkConfiguration getDefaultMax(int id, boolean inverted, SparkMax followerOf) {
+    SparkConfiguration sc = new SparkConfiguration(id, IdleMode.kCoast, inverted, 60, 8, 2,
+        StatusFrames.getDefault(), new SparkMaxConfig());
+    sc.follow(followerOf);
+    return sc;
+  }
 
-    public static final SparkConfiguration getDefaultMax(int id, boolean inverted, SparkMax followerOf) {
-        SparkConfiguration sc = new SparkConfiguration(id, IdleMode.kCoast, inverted, 60, 8, 2,
-                StatusFrames.getDefault(), new SparkMaxConfig());
-        sc.follow(followerOf);
-        return sc;
-    }
+  public static final SparkConfiguration getDefaultFlex(int id) {
+    return new SparkConfiguration(id, IdleMode.kCoast, false, 45, 8, 2, StatusFrames.getDefault(),
+        new SparkFlexConfig());
+  }
 
-    public static final SparkConfiguration getDefaultFlex(int id) {
-        return new SparkConfiguration(id, IdleMode.kCoast, false, 45, 8, 2, StatusFrames.getDefault(),
-                new SparkFlexConfig());
-    }
+  public static final SparkConfiguration getDefaultFlex(int id, boolean inverted) {
+    return new SparkConfiguration(id, IdleMode.kCoast, inverted, 45, 8, 2, StatusFrames.getDefault(),
+        new SparkFlexConfig());
+  }
 
-    public static final SparkConfiguration getDefaultFlex(int id, boolean inverted) {
-        return new SparkConfiguration(id, IdleMode.kCoast, inverted, 45, 8, 2, StatusFrames.getDefault(),
-                new SparkFlexConfig());
-    }
+  public static final SparkConfiguration getDefaultFlex(int id, boolean inverted, SparkFlex followerOf) {
+    SparkConfiguration sc = new SparkConfiguration(id, IdleMode.kCoast, inverted, 60, 8, 2,
+        StatusFrames.getDefault(), new SparkFlexConfig());
+    sc.follow(followerOf);
+    return sc;
+  }
 
-    public static final SparkConfiguration getDefaultFlex(int id, boolean inverted, SparkFlex followerOf) {
-        SparkConfiguration sc = new SparkConfiguration(id, IdleMode.kCoast, inverted, 60, 8, 2,
-                StatusFrames.getDefault(), new SparkFlexConfig());
-        sc.follow(followerOf);
-        return sc;
-    }
+  public static final SparkConfiguration getShooterFlex(int id) {
+    return new SparkConfiguration(id, IdleMode.kCoast, false, 80, 8, 2, StatusFrames.getDefault(),
+        new SparkFlexConfig());
+  }
+  public static final SparkConfiguration getShooterFlex(int id, boolean inverted) {
+    return new SparkConfiguration(id, IdleMode.kCoast, inverted, 80, 8, 2, StatusFrames.getDefault(),
+        new SparkFlexConfig());
+  }
 
-    public static final SparkConfiguration getShooterFlex(int id) {
-        return new SparkConfiguration(id, IdleMode.kCoast, false, 80, 8, 2, StatusFrames.getDefault(),
-                new SparkFlexConfig());
-    }
-
-    public static final SparkConfiguration getShooterFlex(int id, boolean inverted) {
-        return new SparkConfiguration(id, IdleMode.kCoast, inverted, 80, 8, 2, StatusFrames.getDefault(),
-                new SparkFlexConfig());
-    }
-
-    public static final SparkConfiguration getShooterFlex(int id, boolean inverted, SparkFlex followerOf) {
-        SparkConfiguration sc = new SparkConfiguration(id, IdleMode.kCoast, inverted, 80, 8, 2,
-                StatusFrames.getDefault(), new SparkFlexConfig());
-        sc.follow(followerOf, true);
-        return sc;
-    }
-
-    public static final SparkFlex driveFlex(int id) {
-        return SparkConfigurer.configSparkFlex(new SparkConfiguration(id, IdleMode.kBrake, true, 45, 8, 2,
-                new StatusFrames(100, 20, (int) (1000 / DriveConstants.odometryFrequency), 500, 500, 500, 500),
-                new SparkFlexConfig()));
-    }
+  public static final SparkConfiguration getShooterFlex(int id, boolean inverted, SparkFlex followerOf) {
+    SparkConfiguration sc = new SparkConfiguration(id, IdleMode.kCoast, inverted, 80, 8, 2,
+        StatusFrames.getDefault(), new SparkFlexConfig());
+    sc.follow(followerOf, true);
+    return sc;
+  }
+  public static final SparkFlex driveFlex(int id) {
+    return SparkConfigurer.configSparkFlex(new SparkConfiguration(id, IdleMode.kBrake, true, 45, 8, 2,
+        new StatusFrames(100, 20, (int) (1000 / DriveConstants.odometryFrequency), 500, 500, 500, 500),
+        new SparkFlexConfig()));
+  }
 }
