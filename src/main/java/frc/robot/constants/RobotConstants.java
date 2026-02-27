06ebd141aa0c064d9b4a5a7743ee60cd14124c1d
tgschmale <tyguysuper@gmail.com>
2026-02-27 18:28:07 -0500

    this will inevitably cause merge conflicts.

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -39,7 +39,7 @@ public class RobotConstants {
     // ROBOTS
     public static final RobotType duex26 = new RobotType("Duex26", driveSubsystem, kickerSubsystem,
         spindexerSubsystem, hoodSubsystem, shooterSubsystem, turretSubsystem,
-        intakeRollerSubsystem);
+        intakeRollerSubsystem).addAprilTagCamera(CameraSettings.deuxBackCamera).addAprilTagCamera(CameraSettings.deuxRightCamera);
 
     public static final RobotType frank25 = new RobotType("Frank25", driveSubsystem)
         .addAprilTagCamera(CameraSettings.frankOdometryCamera);
@@ -92,11 +92,19 @@ public class RobotConstants {
                 -Math.PI / 2)/* .rotateBy(new Rotation3d(73 * Math.PI / 180, 0, 0)) */),
         1, "Park", "Right Reef Camera");
 
-    public static final CameraConstant duexLeftBackCamera = new CameraConstant(new SimCameraProperties(),
-        new Transform3d(new Translation3d(Units.inchesToMeters(-12.325), Units.inchesToMeters(-7.25), Units
-            .inchesToMeters(6)), new Rotation3d(Math.toDegrees(73),0,20)/* .rotateBy(new Rotation3d(73 * Math.PI / 180, 0, 0)) */),
-        1, "Arducam_OV2311_USB_Camera", "Turret Right Side (From Forward) Camera");
-  }
+    public static final CameraConstant deuxRightCamera = new CameraConstant(new SimCameraProperties(),
+        new Transform3d(new Translation3d(Units.inchesToMeters(-3.7), Units.inchesToMeters(-13.57), Units
+            .inchesToMeters(8.875)), new Rotation3d(0, Math.PI / 4,
+                -Math.PI / 2)/* .rotateBy(new Rotation3d(73 * Math.PI / 180, 0, 0)) */),
+        1, "N/A", "N/A"); // TODO: Change network and display name
+
+    public static final CameraConstant deuxBackCamera = new CameraConstant(new SimCameraProperties(),
+        new Transform3d(new Translation3d(Units.inchesToMeters(-13.7), Units.inchesToMeters(-9.95), Units
+            .inchesToMeters(11.5)), new Rotation3d(0, Math.PI / 18,
+                Math.PI)/* .rotateBy(new Rotation3d(73 * Math.PI / 180, 0, 0)) */),
+        1, "N/A", "N/A"); // TODO: Change network and display name
+    // TODO: Add last camera once possible
+      }
 
   public static class WarningThresholdConstants {
 
