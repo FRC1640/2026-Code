8d4a4d6a237bee59a8fbd52c09d068cbb32bbfaa
smathew01 <mathewshaun25@gmail.com>
2026-02-28 15:28:53 -0500

    food

--- src/main/java/frc/robot/constants/RobotPIDConstants.java
+++ src/main/java/frc/robot/constants/RobotPIDConstants.java
@@ -31,8 +31,8 @@ public class RobotPIDConstants {
 
   public static final PIDConstants turretAnglePidSim = new PIDConstants(0.6, 0, 0);
   public static final PIDConstants turretVelocityPidSim = new PIDConstants(0.06, 0, 0);
-  public static final PIDConstants turretAnglePidReal = new PIDConstants(2, 0, 0);
-  public static final Constraints turretAngleConstraintsReal = new Constraints(Math.PI/4, Math.PI/8);
+  public static final PIDConstants turretAnglePidReal = new PIDConstants(2.1, 0, 0);
+  public static final Constraints turretAngleConstraintsReal = new Constraints(Math.PI/4, Math.PI/5);
   public static final FeedForwardConstants turretAngleFF = new FeedForwardConstants(0, 0/*1 / 1.06*/, 0);
   public static final PIDConstants shooterVelocityPidSim = new PIDConstants(0.005, 0, 0);
   public static final FeedForwardConstants shooterVelocityFFSim = new FeedForwardConstants(0, 0.02, 0);
