42b4506b90415fd43e538e25bc0e5e778e7b917c
QmWn1495 <npanxxxxx@gmail.com>
2026-03-01 13:24:36 -0500

    why

--- src/main/java/frc/robot/constants/FieldConstants.java
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -82,20 +82,20 @@ public class FieldConstants {
   //we should add moon and sun to account for the tides
   public static final double gravityEarth = 9.80665;
 
-  public static final PoseFilter rightTrench = new PoseFilter(new Translation2d(1,0), new Pose2d(4.125594, 0, new Rotation2d()))
+  public static final PoseFilter rightBlueTrench = new PoseFilter(new Translation2d(1,0), new Pose2d(4.125594, 0, new Rotation2d()))
   .addFilter(new Translation2d(-1,0), new Pose2d(5.125594, 0, new Rotation2d()))
   .addFilter(new Translation2d(0,1), new Pose2d(0, 0, new Rotation2d()))
   .addFilter(new Translation2d(0,-1), new Pose2d(0, 1.281938, new Rotation2d()));
-  public static final PoseFilter leftTrench = new PoseFilter(new Translation2d(1,0), new Pose2d(4.125594, 0, new Rotation2d()))
+  public static final PoseFilter leftBlueTrench = new PoseFilter(new Translation2d(1,0), new Pose2d(4.125594, 0, new Rotation2d()))
   .addFilter(new Translation2d(-1,0), new Pose2d(5.125594, 0, new Rotation2d()))
   .addFilter(new Translation2d (0, 1), new Pose2d(0, 6.7858132, new Rotation2d()))
   .addFilter(new Translation2d(0, -1), new Pose2d(0, 8.0677512, new Rotation2d()));
-  public static final PoseFilter rightBump = new PoseFilter(new Translation2d(1, 0), new Pose2d(4.028694, 0, new Rotation2d()))
-  .addFilter(new Translation2d(-1, 0), new Pose2d(5.222494, 0, new Rotation2d()))
+  public static final PoseFilter rightRedTrench = new PoseFilter(new Translation2d(1, 0), new Pose2d(11.318494, 0, new Rotation2d()))
+  .addFilter(new Translation2d(-1, 0), new Pose2d(12.512294, 0, new Rotation2d()))
   .addFilter(new Translation2d(0, 1), new Pose2d(0, 1.606886, new Rotation2d()))
   .addFilter(new Translation2d(0, -1), new Pose2d(0, 3.426886, new Rotation2d()));
-  public static final PoseFilter leftBump = new PoseFilter(new Translation2d(1, 0), new Pose2d(4.028694, 0, new Rotation2d()))
-  .addFilter(new Translation2d(-1, 0), new Pose2d(5.222494, 0, new Rotation2d()))
+  public static final PoseFilter leftRedTrench = new PoseFilter(new Translation2d(1, 0), new Pose2d(11.318494, 0, new Rotation2d()))
+  .addFilter(new Translation2d(-1, 0), new Pose2d(12.512294, 0, new Rotation2d()))
   .addFilter(new Translation2d(0, 1), new Pose2d(0, 4.6423892, new Rotation2d() ))
   .addFilter(new Translation2d(0, -1), new Pose2d(0, 6.4623892, new Rotation2d()));
 }
