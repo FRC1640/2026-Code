e8b7552d28698b70202bc1dd137a60c8a8c17c00
1023941311414 <jake.widmann@outlook.com>
2026-03-01 21:42:56 -0500

    Removed shooter todos and raised shooter current limit

--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -101,12 +101,12 @@ public class ShooterSubsystem extends SubsystemPlatform {
   public boolean isAtSetpoint() {
     double currentRPM = (inputs.leaderVelocityRPM + inputs.followerVelocityRPM) * 0.5;
     double setpointRPM = ShotControl.getInstance().getSetpoint().shooterVelocityRPM();
-    return Math.abs(currentRPM - setpointRPM) < ShooterConstants.setpointVelocityToleranceRPM; // TODO: tune
+    return Math.abs(currentRPM - setpointRPM) < ShooterConstants.setpointVelocityToleranceRPM;
   }
   
   public boolean isAtTestSetpoint() {
     double currentRPM = (inputs.leaderVelocityRPM + inputs.followerVelocityRPM) * 0.5;
-    return Math.abs(currentRPM - testVelocityRPM) < ShooterConstants.setpointVelocityToleranceRPM; // TODO: tune
+    return Math.abs(currentRPM - testVelocityRPM) < ShooterConstants.setpointVelocityToleranceRPM;
   }
 
   @Override
