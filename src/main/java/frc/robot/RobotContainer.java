c807cb0f64e6c5ce6e41a30641e034f64a24bf0b
QmWn1495 <npanxxxxx@gmail.com>
2026-03-03 19:03:30 -0500

    why am i still here

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,7 @@ import org.littletonrobotics.junction.Logger;
 
 import com.pathplanner.lib.auto.NamedCommands;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.Alert.AlertType;
@@ -125,10 +126,10 @@ public class RobotContainer {
         () -> -driveController.getRightX(), () -> driveController.getRightTriggerAxis() > 0.1,
         () -> driveController.getLeftTriggerAxis() > 0.1, () -> true, gyro, () -> false,
         () -> driveController.b().getAsBoolean()
-            && (DistanceManager.inRange(LockToPoint.activeDistanceX,
-                lockToPointWeight.getTargetPoint().getY(), lockToPointWeight.getRobotPose().getY()))
-            && (DistanceManager.inRange(LockToPoint.activeDistanceY,
-                lockToPointWeight.getTargetPoint().getX(), lockToPointWeight.getRobotPose().getX())));
+            && (MathUtil.isNear(lockToPointWeight.getTargetPoint().getY(), 
+            lockToPointWeight.getRobotPose().getY(), LockToPoint.activeDistanceX))
+            && (MathUtil.isNear(lockToPointWeight.getTargetPoint().getX(), 
+            lockToPointWeight.getRobotPose().getX(), LockToPoint.activeDistanceY)));
     DriveWeightCommand.addPersistentWeight(joystickDriveWeight);
     driveToPointWeight = new DriveToPoint(() -> RobotOdometry.instance.getPose("Main"), () -> new Pose2d(
         AllianceManager.chooseFromAlliance(FieldConstants.blueTowerBarCenter, FieldConstants.redTowerBarCenter),
@@ -167,10 +168,10 @@ public class RobotContainer {
     DriveWeightCommand.createWeightTrigger(driveToPointWeight, () -> driveController.a().getAsBoolean());
     DriveWeightCommand.createWeightTrigger(lockToPointWeight,
         () -> driveController.b().getAsBoolean()
-            && (DistanceManager.inRange(LockToPoint.activeDistanceX,
-                lockToPointWeight.getTargetPoint().getY(), lockToPointWeight.getRobotPose().getY()))
-            && (DistanceManager.inRange(LockToPoint.activeDistanceY,
-                lockToPointWeight.getTargetPoint().getX(), lockToPointWeight.getRobotPose().getX())));
+            && (MathUtil.isNear(lockToPointWeight.getTargetPoint().getY(), 
+            lockToPointWeight.getRobotPose().getY(), LockToPoint.activeDistanceX))
+            && (MathUtil.isNear(lockToPointWeight.getTargetPoint().getX(), 
+            lockToPointWeight.getRobotPose().getX(), LockToPoint.activeDistanceY)));
   }
 
   private void generateTriggers() {
