6ee8a27912c1034f1310c4015f443b144f9df7ac
Kgupta011 <gimonika82@gmail.com>
2026-03-05 19:51:47 -0500

    intake

--- src/main/java/frc/robot/subsystems/intakeRollers/IntakeRollerConstants.java
+++ src/main/java/frc/robot/subsystems/intakeRollers/IntakeRollerConstants.java
@@ -5,7 +5,7 @@ import frc.robot.util.robotswitcher.SwitchableCANID;
 public class IntakeRollerConstants {
   public static final int canID = SwitchableCANID.of(16).get();
   public static final double gearRatio = 1;
-  public static final double intakeVoltage = 1; // TODO: change
+  public static final double intakeVoltage = -8; 
   public static final double intakeCurrentLimitAmps = Double.POSITIVE_INFINITY; // TODO: change
 
 }
