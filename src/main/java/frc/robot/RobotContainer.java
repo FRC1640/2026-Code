3b8cd56fe780d650447553548004c6cdb7056ec3
1023941311414 <jake.widmann@outlook.com>
2026-03-12 23:09:31 -0400

    Moved Shot Control initialization (maybe things will get better?)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -156,7 +156,6 @@ public class RobotContainer {
     // general robot config
     bumpDetector = new BumpDetectorPeriodic(gyro, 3, Math.PI / 36);
     new RobotOdometry(driveSubsystem, gyro, visionArray).setBumpDetector(bumpDetector);
-    new ShotControl(() -> RobotOdometry.instance.getPose("Main"), () -> driveSubsystem.getChassisSpeeds());
     robotCommands = new RobotCommands(shooterSubsystem, kickerSubsystem, spindexerSubsystem, intakeSubsystem,
         intakeRollerSubsystem, hoodSubsystem, turretSubsystem, driveSubsystem);
     alertsManager = new AlertsManager();
@@ -171,6 +170,7 @@ public class RobotContainer {
     driveSubsystem.configurePathplanner();
 
     shotCorrectionWeight = new ShotCorrectionWeight(turretSubsystem);
+    new ShotControl(() -> RobotOdometry.instance.getPose("Main"), () -> driveSubsystem.getChassisSpeeds());
 
     PeriodicScheduler.getInstance().addPeriodic(new PeriodicBase() {
       @Override
