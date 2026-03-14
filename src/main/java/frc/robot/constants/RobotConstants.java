abd3c28f7fd552970667c4830c312317dec6cf52
1640driverstation <lebermanstation@hotmail.com>
2026-03-14 17:11:24 -0400

    Update at 'Sat Mar 14 17:11:24 EDT 2026'

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -43,8 +43,8 @@ public class RobotConstants {
     public static final RobotType prime26 = new RobotType("Prime26", driveSubsystem, hoodSubsystem,
         intakeRollerSubsystem, turretSubsystem, shooterSubsystem, spindexerSubsystem, kickerSubsystem,
         intakeSubsystem).addAprilTagCamera(CameraConstants.primeLeftCamera)
-            .addAprilTagCamera(CameraConstants.primeRightCamera);
-    // .addAprilTagCamera(CameraConstants.primeBackCamera);
+            .addAprilTagCamera(CameraConstants.primeRightCamera)
+            .addAprilTagCamera(CameraConstants.primeBackCamera);
     public static final RobotType duex26 = new RobotType("Duex26", driveSubsystem, kickerSubsystem,
         spindexerSubsystem, intakeSubsystem, intakeRollerSubsystem, turretSubsystem, shooterSubsystem,
         hoodSubsystem).addAprilTagCamera(CameraConstants.deuxRightCamera)
@@ -154,7 +154,7 @@ public class RobotConstants {
     public static final CameraConstant primeBackCamera = new CameraConstant(new SimCameraProperties(),
         new Transform3d(new Translation3d(-0.33998 + xoffset, -0.24858, 0.2 + zoffset),
             new Rotation3d(0, -Math.PI / 9, Math.PI)),
-        1, "Dodds"/* "Arducam_OV9281_USB_Camera" */, "Prime Back Camera");
+        1, "Arducam_OV2311_USB_Camera"/*"Dodds"*//* "Arducam_OV9281_USB_Camera" */, "Prime Back Camera");
 
     /**
      * Left prime camera, mounted on turret base.
