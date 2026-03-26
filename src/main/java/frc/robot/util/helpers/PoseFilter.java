85e28bac7994d5b17f7adde289169c9715f89207
Shaun Mathew <143555953+SpeedSlicer@users.noreply.github.com>
2026-03-26 18:42:59 -0400

    i think this is how I'm supposed to do something

--- src/main/java/frc/robot/util/helpers/PoseFilter.java
+++ src/main/java/frc/robot/util/helpers/PoseFilter.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BooleanSupplier;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -38,95 +37,20 @@ public class PoseFilter {
   }
 
   public class PoseFilterPreset {
-    public static Function<Pose2d, BooleanSupplier> isPoseAboveOf(Supplier<Pose2d> pose) {
-      return new Function<Pose2d, BooleanSupplier>() {
-        @Override
-        public BooleanSupplier apply(Pose2d t) {
-          return () -> DistanceManager.isAboveOf(t, pose.get());
-        }
-      };
-    }
-
-    public static Function<Pose2d, BooleanSupplier> isPoseBelowOf(Supplier<Pose2d> pose) {
-      return new Function<Pose2d, BooleanSupplier>() {
-        @Override
-        public BooleanSupplier apply(Pose2d t) {
-          return () -> DistanceManager.isBelowOf(t, pose.get());
-        }
-      };
-    }
-
-    public static Function<Pose2d, BooleanSupplier> isPoseRightOf(Supplier<Pose2d> pose) {
-      return new Function<Pose2d, BooleanSupplier>() {
-        @Override
-        public BooleanSupplier apply(Pose2d t) {
-          return () -> DistanceManager.isRightOf(t, pose.get());
-        }
-      };
-    }
-
-    public static Function<Pose2d, BooleanSupplier> isPoseLeftOf(Supplier<Pose2d> pose) {
-      return new Function<Pose2d, BooleanSupplier>() {
-        @Override
-        public BooleanSupplier apply(Pose2d t) {
-          return () -> DistanceManager.isLeftOf(t, pose.get());
-        }
-      };
-    }
-
-    public static Function<Pose2d, BooleanSupplier> vectorFilterFunction(Supplier<Translation2d> trans,
-        Supplier<Pose2d> pose) {
-      return new Function<Pose2d, BooleanSupplier>() {
-        @Override
-        public BooleanSupplier apply(Pose2d t) {
-          return () -> (((t.getTranslation().minus(pose.get().getTranslation())).dot(trans.get())) > 0);
-        }
-      };
-    }
-
-    public static Function<Pose2d, BooleanSupplier> inRectZone(Supplier<Pose2d> point1, Supplier<Pose2d> point2) {
-      return new Function<Pose2d, BooleanSupplier>() {
-        @Override
-        public BooleanSupplier apply(Pose2d t) {
-          return () -> DistanceManager.isBetweenPose(point1.get(), point2.get(), t);
-        }
-      };
-    }
-
     public static Function<Pose2d, BooleanSupplier> isPoseAboveOf(Pose2d pose) {
-      return new Function<Pose2d, BooleanSupplier>() {
-        @Override
-        public BooleanSupplier apply(Pose2d t) {
-          return () -> DistanceManager.isAboveOf(t, pose);
-        }
-      };
+      return PoseFilterPreset.vectorFilterFunction(new Translation2d(0, 1), pose);
     }
 
     public static Function<Pose2d, BooleanSupplier> isPoseBelowOf(Pose2d pose) {
-      return new Function<Pose2d, BooleanSupplier>() {
-        @Override
-        public BooleanSupplier apply(Pose2d t) {
-          return () -> DistanceManager.isBelowOf(t, pose);
-        }
-      };
+      return PoseFilterPreset.vectorFilterFunction(new Translation2d(0, -1), pose);
     }
 
     public static Function<Pose2d, BooleanSupplier> isPoseRightOf(Pose2d pose) {
-      return new Function<Pose2d, BooleanSupplier>() {
-        @Override
-        public BooleanSupplier apply(Pose2d t) {
-          return () -> DistanceManager.isRightOf(t, pose);
-        }
-      };
+      return PoseFilterPreset.vectorFilterFunction(new Translation2d(1, 0), pose);
     }
 
     public static Function<Pose2d, BooleanSupplier> isPoseLeftOf(Pose2d pose) {
-      return new Function<Pose2d, BooleanSupplier>() {
-        @Override
-        public BooleanSupplier apply(Pose2d t) {
-          return () -> DistanceManager.isLeftOf(t, pose);
-        }
-      };
+      return PoseFilterPreset.vectorFilterFunction(new Translation2d(-1, 0), pose);
     }
 
     public static Function<Pose2d, BooleanSupplier> vectorFilterFunction(Translation2d trans, Pose2d pose) {
