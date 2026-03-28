95b453c400e3bee38ed2cfc0bc72dbbb57a06437
1640driverstation <lebermanstation@hotmail.com>
2026-03-28 11:30:11 -0400

    Update at 'Sat Mar 28 11:30:10 EDT 2026'

--- src/main/java/frc/robot/subsystems/hood/HoodConstants.java
+++ src/main/java/frc/robot/subsystems/hood/HoodConstants.java
@@ -10,7 +10,7 @@ public class HoodConstants {
   public static final int canId = SwitchableCANID.of(12).get();
 
   /** Hood angle limits, with the horizontal. */
-  public static final Limits angleLimitsRadians = new Limits(Units.degreesToRadians(17), Units.degreesToRadians(31),
+  public static final Limits angleLimitsRadians = new Limits(Units.degreesToRadians(17), Units.degreesToRadians(29),
       true);
 
   /**
