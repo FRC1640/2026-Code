d672870afed8a71c864da8282e961e8dc51d6297
QmWn1495 <npanxxxxx@gmail.com>
2026-04-07 20:45:10 -0400

    Merge branches 'feature/delete-mathews-code' and 'master' of https://github.com/FRC1640/2026-Code

--- src/main/java/frc/robot/util/logging/PeriodicLogging.java
+++ src/main/java/frc/robot/util/logging/PeriodicLogging.java
@@ -8,17 +8,20 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.constants.FieldConstants;
 import frc.robot.constants.RobotConstants;
 import frc.robot.sensors.odometry.RobotOdometry;
+import frc.robot.subsystems.ShotControl;
+import frc.robot.subsystems.turret.TurretConstants;
 import frc.robot.util.helpers.AllianceManager;
 import frc.robot.util.periodic.PeriodicBase;
 
 public class PeriodicLogging extends PeriodicBase {
 
   public boolean active;
-  public boolean initial = false;
+  public boolean initial;
   private String alliance;
   private final Field2d m_field = new Field2d();
   public PeriodicLogging() {
     active = false;
+    alliance = AllianceManager.chooseFromAlliance("B", "R");
     SmartDashboard.putData("Field", m_field);
   }
 
@@ -35,46 +38,53 @@ public class PeriodicLogging extends PeriodicBase {
     }
   }
 
-  public boolean getActive() {
-
-    if (DriverStation.isAutonomous()) {
-      active = true;
-    } else if (130 < DriverStation.getMatchTime() && DriverStation.getMatchTime() < 140) {
-      active = true;
-    } else if (30 < DriverStation.getMatchTime() && DriverStation.getMatchTime() < 130) {
-      int period = (int) ((DriverStation.getMatchTime() - 30) / 25);
-      if (period % 2 == 1) {
-        active = initial;
-      } else {
-        active = !initial;
-      }
-    } else {
-      active = true;
-    }
-    return active;
-  }
-
-  public double getRemainingPeriodTime() {
-    if (DriverStation.isAutonomous() || DriverStation.getMatchTime() < 30) {
-      return DriverStation.getMatchTime();
-    } else {
-      return (DriverStation.getMatchTime() - 30) % 25;
-    }
-  }
-
-  @Override
-  public void periodic() {
+  public boolean getActive(double matchTime) {
     String gameData = DriverStation.getGameSpecificMessage();
-    alliance = AllianceManager.chooseFromAlliance("B", "R");
-    if (137 < DriverStation.getMatchTime() && DriverStation.getMatchTime() < 140 && !gameData.isEmpty()) {
+    if (DriverStation.isAutonomous()) {
+      active = false;
+    } else if (137 < matchTime && matchTime < 140) {
       if (gameData.charAt(0) == 'R' || gameData.charAt(0) == 'B') {
         initial = gameData.charAt(0) != alliance.charAt(0);
       } else {
         initial = false;
       }
+      active = false;
+    } else if (130 < matchTime && matchTime < 137) {
+      active = false;
+    } else {
+      int period = (int) ((matchTime - 30) / 25);
+      if (period % 2 == 1) {
+        active = initial;
+      } else {
+        active = !initial;
+      }
     }
-    Logger.recordOutput("Dashboard/IsActivePeriod", getActive());
-    Logger.recordOutput("Dashboard/InitialPeriod", initial);
+    return active;
+  }
+
+  public double getRemainingPeriodTime() {
+    return (DriverStation.getMatchTime() - 30) % 25;
+  }
+  
+  public boolean canShoot(double matchTime){
+    if(AllianceManager.chooseFromAlliance(1,2)==1){
+      double timeOfFlight = ShotControl.AZInterpolator
+      .getTimeOfFlight(FieldConstants.hubPositionBlue.getTranslation()
+      .getDistance(RobotOdometry.instance.getPose("Main")
+      .plus(TurretConstants.turretTransform2d).getTranslation()));
+      return getActive(DriverStation.getMatchTime()-timeOfFlight);
+    } else {
+      double timeOfFlight = ShotControl.AZInterpolator
+      .getTimeOfFlight(FieldConstants.hubPositionRed.getTranslation()
+      .getDistance(RobotOdometry.instance.getPose("Main")
+      .plus(TurretConstants.turretTransform2d).getTranslation()));
+      return getActive(DriverStation.getMatchTime()-timeOfFlight);
+    }
+  }
+
+  @Override
+  public void periodic() {
+    Logger.recordOutput("Dashboard/IsActivePeriod", getActive(DriverStation.getMatchTime()));
     Logger.recordOutput("Dashboard/RemainingPeriodTime", getRemainingPeriodTime());
     Logger.recordOutput("Dashboard/MatchTime", DriverStation.getMatchTime());
     Logger.recordOutput("Dashboard/GameSpecificMessage", DriverStation.getGameSpecificMessage());
