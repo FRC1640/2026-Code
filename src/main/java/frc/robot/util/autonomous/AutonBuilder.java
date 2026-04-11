48a89920c68e713992623e781957ffd88c34fcd9
Muthu Subhash Muthu <m.muthusubhash@gmail.com>
2026-04-11 18:06:47 -0400

    bam.

--- src/main/java/frc/robot/util/autonomous/AutonBuilder.java
+++ src/main/java/frc/robot/util/autonomous/AutonBuilder.java
@@ -89,21 +89,16 @@ public class AutonBuilder {
                     .withTimeout(8))),
               new WaitCommand(8)),
           new Path("center 1"), robotCommands));
+      
+    // 2056 Outpost: Trench -> Sweep -> Bump -> S.W.I.M -> Trench -> Sweep -> Bump -> S.W.I.M -> Trench
     autons.put("2056 Outpost",
         new Auton(
           Commands.sequence(
               pathBuilder.build(new Path("2056 path 1")),
-              new InstantCommand(() -> CommandScheduler.getInstance().schedule(
-                  robotCommands.autoShootCommand()
-                    .alongWith(robotCommands.autoOscillateCommand(0.75))
-                    .withTimeout(8))),
-              new WaitCommand(6),
+              new WaitCommand(2),
               pathBuilder.build(new Path("2056 path 2")),
-              new InstantCommand(() -> CommandScheduler.getInstance().schedule(
-                  robotCommands.autoShootCommand()
-                    .alongWith(robotCommands.autoOscillateCommand(0.75))
-                    .withTimeout(8))),
-              new WaitCommand(8)),
+              new WaitCommand(2)
+          ),
           new Path("2056 path 1"), robotCommands));
 
 
