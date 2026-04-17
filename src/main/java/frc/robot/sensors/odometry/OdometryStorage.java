7f48c77d109e1be5dec9e54b4de9704fb22ddf94
1640driverstation <lebermanstation@hotmail.com>
2026-04-17 10:28:43 -0400

    Update at 'Fri Apr 17 10:28:43 EDT 2026'

--- src/main/java/frc/robot/sensors/odometry/OdometryStorage.java
+++ src/main/java/frc/robot/sensors/odometry/OdometryStorage.java
@@ -36,7 +36,7 @@ public class OdometryStorage {
   private double visionStdDevCompensation = 1;
   private final double trustResetDistanceThreshold = 0.04;
 
-  private boolean clampPoseInField = false;
+  private boolean clampPoseInField = true;
   private DoubleSupplier clampingRotation = null;
 
   private OdometryStorage trustedRotation = null;
