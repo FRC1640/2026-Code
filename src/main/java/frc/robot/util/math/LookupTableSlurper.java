33a0166b834e9dfb9124814c96b503a53db13a8a
Muthu Subhash Muthu <m.muthusubhash@gmail.com>
2026-04-21 23:29:58 -0400

    true slurp

--- src/main/java/frc/robot/util/math/LookupTableSlurper.java
+++ src/main/java/frc/robot/util/math/LookupTableSlurper.java
@@ -3,7 +3,12 @@ package frc.robot.util.math;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class LookupTableSlurper {
   public enum LookupTableType {
@@ -12,31 +17,30 @@ public class LookupTableSlurper {
 
   public static HashMap<LookupTableType, String> FileMap = new HashMap<LookupTableType, String>() {
     {
-      put(LookupTableType.PRIME26AZ, "src/main/resources/lookuptables/prime26az.csv");
-      put(LookupTableType.PRIME26NZ, "src/main/resources/lookuptables/prime26nz.csv");
-      put(LookupTableType.DEUX26AZ, "src/main/resources/lookuptables/deux26az.csv");
-      put(LookupTableType.DEUX26NZ, "src/main/resources/lookuptables/deux26nz.csv");
+      put(LookupTableType.PRIME26AZ, "prime26az.csv");
+      put(LookupTableType.PRIME26NZ, "prime26nz.csv");
+      put(LookupTableType.DEUX26AZ, "deux26az.csv");
+      put(LookupTableType.DEUX26NZ, "deux26nz.csv");
     }
   };
 
   public static ShotInterpolator slurpShotInterpolator(LookupTableType tableType) {
     ShotInterpolator interpolator = new ShotInterpolator();
 
-    String line = "";
     String filePath = FileMap.get(tableType);
+    try {
+      Files.readAllLines(Paths.get(filePath))
+          .stream()
+          .map(line -> line.split(","))
+          .collect(Collectors.toList())
+          .forEach(values -> {
+            double distance = Double.parseDouble(values[0]);
+            double hoodAngle = Double.parseDouble(values[1]);
+            double shooterVelocity = Double.parseDouble(values[2]);
+            double timeOfFlight = Double.parseDouble(values[3]);
+            interpolator.put(distance, hoodAngle, shooterVelocity, timeOfFlight);
 
-    try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-      while ((line = br.readLine()) != null) {
-
-        String[] values = line.split(",");
-
-        double distance = Double.parseDouble(values[0]);
-        double hoodAngle = Double.parseDouble(values[1]);
-        double shooterVelocity = Double.parseDouble(values[2]);
-        double timeOfFlight = Double.parseDouble(values[3]);
-        interpolator.put(distance, hoodAngle, shooterVelocity, timeOfFlight);
-
-      }
+          });
     } catch (IOException e) {
       e.printStackTrace();
     }
