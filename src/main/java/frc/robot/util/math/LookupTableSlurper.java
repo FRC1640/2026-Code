b0a0cf1c2236879fe467d6e54c147d08eaf85da4
Muthu Subhash Muthu <m.muthusubhash@gmail.com>
2026-04-21 23:48:23 -0400

    make sure to slurp properly

--- src/main/java/frc/robot/util/math/LookupTableSlurper.java
+++ src/main/java/frc/robot/util/math/LookupTableSlurper.java
@@ -1,14 +1,9 @@
 package frc.robot.util.math;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class LookupTableSlurper {
   public enum LookupTableType {
