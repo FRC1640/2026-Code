12f1757b73e5edaa12d408fad667f9bd67fe4508
1640driverstation <lebermanstation@hotmail.com>
2026-04-25 16:34:34 -0400

    modified:   src/main/java/frc/robot/subsystems/spindexer/SpindexerSubsystem.java

--- src/main/java/frc/robot/subsystems/spindexer/SpindexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/spindexer/SpindexerSubsystem.java
@@ -29,7 +29,7 @@ public class SpindexerSubsystem extends SubsystemPlatform {
   ----------*/
 
   public Command runCommand() {
-    return runVelocityRPMCommand(() -> 3600);
+    return runVelocityRPMCommand(() -> 4500);
   }
 
   public Command runVelocityRPMCommand(DoubleSupplier velocityRPM) {
