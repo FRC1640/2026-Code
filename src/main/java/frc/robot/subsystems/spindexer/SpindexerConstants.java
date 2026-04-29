6ee9f4a48c5ea573bc9333ad4d2c7bb337de1029
1640driverstation <lebermanstation@hotmail.com>
2026-04-29 17:04:28 -0400

    Update at 'Wed Apr 29 17:04:28 EDT 2026'

--- src/main/java/frc/robot/subsystems/spindexer/SpindexerConstants.java
+++ src/main/java/frc/robot/subsystems/spindexer/SpindexerConstants.java
@@ -11,6 +11,6 @@ public class SpindexerConstants {
   public static final double indexerGearRatioSim = 1;
 
   public static final double runVoltage = 8;
-  public static final double runVelocity = 4000; // 3150 rpm
+  public static final double runVelocity = 3850; // 3150 rpm
   public static final double jamCurrentThresh = 60;
 }
